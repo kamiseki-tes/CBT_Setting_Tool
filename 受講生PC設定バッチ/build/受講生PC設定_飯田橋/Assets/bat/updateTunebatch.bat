9fbc1469020ee5867f829127a9b8a386378eb5dd
ShimizuPC <kamiseki@skn.co.jp>
2026-01-22 09:24:40 +0900

    merge前の準備

--- 受講生PC設定バッチ/build/受講生PC設定_飯田橋/Assets/bat/updateTunebatch.bat
+++ 受講生PC設定バッチ/build/受講生PC設定_飯田橋/Assets/bat/updateTunebatch.bat
@@ -3,17 +3,18 @@
 
 echo *****************************************************
 echo 設定用バッチを更新します(タスクスケジューラからの定期実行)
+echo 飯田橋用はサーバーアドレス、参照階層が異なるので注意
 echo v1.00
 echo *****************************************************
 echo;
 
-@REM 接続先サーバーアドレス（新宿）
-set ipStr=192.168.100.8
+@REM 接続先サーバーアドレス（飯田橋）
+set ipStr=192.168.10.2
 @REM ネットワークドライブの割り当て
 net use Y: \\%ipStr%\共有
 
 @REM サーバーの所定ディレクトリからバッチをコピーする（最新化）
-xcopy /y Y:\maintenance\bat\tuneTrainingPC.bat C:\maintenanceBat\Assets\bat
+xcopy /y Y:\PC設定用\maintenance\bat\tuneTrainingPC.bat C:\maintenanceBat\Assets\bat
 
 @REM rem ネットワークドライブの除去
 net use Y: /delete /y
